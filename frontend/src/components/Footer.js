ec03f93074335ae631bbd3d6cf1fc6c61ceb95a6
Mikhail Koryakin <sdrr87@gmail.com>
2023-08-04 15:27:07 +0300

    feat: branch backend

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  const currentYear = new Date().getFullYear();
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">© {currentYear} Mesto Russia</p>
-    </footer>
-  );
-}
-export default Footer;
