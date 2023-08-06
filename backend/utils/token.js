33a3a76fb12deba4d5cdfec588d6e030ccf0d7a5
Mikhail Koryakin <sdrr87@gmail.com>
2023-08-06 18:10:10 +0300

    feat: esling fix, port fix

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -6,15 +6,4 @@ function generateToken(payload) {
   return JWT.sign(payload, SECRET_KEY, { expiresIn: '7d' });
 }
 
-function checkToken(token) {
-  if (!token) {
-    return false;
-  }
-  try {
-    return JWT.verify(token, SECRET_KEY)
-  } catch {
-    return false;
-  }
-};
-
-module.exports = {generateToken, checkToken}
+module.exports = { generateToken };
