fba99d0c49c2f85245e19c8a661fe1620207bd31
Mikhail Koryakin <sdrr87@gmail.com>
2023-08-07 21:05:48 +0300

    feat: url regExp fix

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -3,7 +3,7 @@ const STATUS_OK = 201;
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
-const URL_REG_EXP = /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/;
+const URL_REG_EXP = /^(https?:\/\/)?([\w\.-]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/;
 const EMAIL_REG_EXP = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
 
 const allowedCors = [
